b838f9dead00bf3bf467ebc0ce135c039df43609
afrajwho <afrajrahman@gmail.com>
2024-12-25 17:36:03 +0530

    Strapi Backend

---
+++ Strapi Backend/AI-Resume-Builder/types/generated/components.d.ts
@@ -0,0 +1,59 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    description: '';
+    displayName: 'Education';
+    icon: 'book';
+  };
+  attributes: {
+    degree: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    universityName: Schema.Attribute.String;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    description: '';
+    displayName: 'Experience';
+    icon: 'briefcase';
+  };
+  attributes: {
+    city: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    endDate: Schema.Attribute.Date;
+    startDate: Schema.Attribute.Date;
+    state: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    workSummery: Schema.Attribute.Blocks;
+  };
+}
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    description: '';
+    displayName: 'Skills';
+    icon: 'apps';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
+    }
+  }
+}
